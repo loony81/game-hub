ae27bfe8f4828ef4a67c6bd50aeaa37adb658938
Roman Nazarenko <r.nazarenko.81@gmail.com>
2023-07-01 14:55:14 +0300

    Show loading skeletons while game cards are being downloaded

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,17 +24,23 @@ export interface Game {
 const useGames = () =>{
     const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     const controller = new AbortController()
+    setLoading(true)
     apiClient
       .get<FetchGamesResponse>("/games", {
         signal: controller.signal
       })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results)
+        setLoading(false)
+      })
       .catch((err) => {
         if(err instanceof CanceledError) return
         setError(err.message)
+        setLoading(false)
       });
 
     return () => controller.abort()
@@ -42,7 +48,8 @@ const useGames = () =>{
 
   return {
     games,
-    error
+    error,
+    isLoading
   }
 }
 
---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+    const media = 'media/'
+    
+    const firstPart = url.slice(0, url.indexOf(media)+ media.length)
+    const lastPart = url.slice(firstPart.length)
+    return firstPart + 'crop/600/400/' + lastPart
+}
+
+export default getCroppedImageUrl
